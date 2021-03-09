13e155b89b71a58d2fba1bbf46884fb403f6ecbc
AndreaBreggion <49444060+AndreaBreggion@users.noreply.github.com>
2021-03-09 18:13:37 +0100

    Update model.js

--- client/src/model/model.js
+++ client/src/model/model.js
@@ -5,7 +5,6 @@
 import * as d3 from 'd3';
 import * as mldistance from 'ml-distance';
 import Dataset from './Dataset';
-import useD3 from '../ui/hooks/useD3';
 
 class Model {
   data = new Dataset();
@@ -166,77 +165,6 @@ class Model {
     this.selectedData.dataFlush();
   }
 
-  getForceField() {
-    const ffvisual = useD3((svg) => {
-      const { nodes } = this.getSelectedData();
-      const { links } = this.getSelectedData();
-
-      const simulation = d3.forceSimulation(nodes)
-        .force('charge', d3.forceManyBody().strength(-100))
-        .force('link', d3.forceLink(links).id((d) => d.id).strength((d) => d.value))
-        .force('center ', d3.forceCenter(300, 300))
-        .on('tick', () => {
-          node
-            .attr('cx', (d) => d.x)
-            .attr('cy', (d) => d.y);
-
-          link
-            .attr('x1', (d) => d.source.x)
-            .attr('y1', (d) => d.source.y)
-            .attr('x2', (d) => d.target.x)
-            .attr('y2', (d) => d.target.y);
-        });
-
-      const link = svg.select('.lines')
-        .attr('stroke', '#999')
-        .attr('stroke-opacity', 0.6)
-        .selectAll('line')
-        .data(links)
-        .join('line')
-        .attr('stroke-width', (d) => d.value);
-
-      const node = svg.select('.circles')
-        .attr('stroke', '#fff')
-        .attr('stroke-width', 1.5)
-        .selectAll('circle')
-        .data(nodes)
-        .join('circle')
-        .attr('r', 5)
-        .attr('fill', (d) => d3.schemeCategory10[d.group])
-        .call(d3.drag(simulation).on('start', (event) => {
-          if (!event.active) simulation.alphaTarget(0.3).restart();
-          event.subject.fx = event.x;
-          event.subject.fy = event.y;
-        })
-          .on('drag', (event) => {
-            event.subject.fx = event.x;
-            event.subject.fy = event.y;
-          })
-          .on('end', (event) => {
-            if (!event.active) simulation.alphaTarget(0);
-            event.subject.fx = null;
-            event.subject.fy = null;
-          }));
-
-      const label = node.append('title')
-        .text((d) => d.label);
-    });
-
-    return (
-      <svg
-        ref={ffvisual}
-        style={{
-          height: 600,
-          width: '100%',
-          marginRight: '0px',
-          marginLeft: '0px',
-        }}
-      >
-        <g className="lines" />
-        <g className="circles" />
-      </svg>
-    );
-  }
 }
 
 /*
