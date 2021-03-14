1b1949024a800a890d25d82618f0190818460e97
Alessandro Flori <flori995@live.com>
2021-03-14 17:36:54 +0100

    Bozza modello senza test

---
+++ client/src/model/DataModel.js
@@ -0,0 +1,73 @@
+class DataModel {
+  constructor(dataset) {
+    this._dataset = dataset;
+  }
+
+  get dataset() {
+    return this._dataset;
+  }
+
+  set dataset(value) {
+    this._dataset = value;
+  }
+
+  get feature() {
+    return this._feature;
+  }
+
+  set feature(value) {
+    this._feature = value;
+  }
+
+  get target() {
+    return this._target;
+  }
+
+  set target(value) {
+    this._target = value;
+  }
+
+  getTargetColumns() {
+    const targetCol = [];
+    return this.dataset.map(
+      (value) => {
+        Object
+          .keys(value) // stream delle chiavi contenute in value (riga del dataset)
+          .filter((key) => this.target.indexOf(key) !== -1)
+          .reduce((obj, key) => ({ // riduce l'array di chiavi in un object literal
+            ...obj,
+            [key]: value[key],
+          }), {});
+      },
+    );
+  }
+
+  getFeatureColumns() {
+    return this.dataset.map(
+      (value) => Object
+        .keys(value) // stream delle chiavi contenute in value (riga del dataset)
+        .filter((key) => this.feature.indexOf(key) !== -1)
+        .reduce((obj, key) => ({ // riduce l'array di chiavi in un object literal
+          ...obj,
+          [key]: value[key],
+        }), {})
+      ,
+    );
+  }
+
+  getSelectedDataset() {
+    return this.dataset.map(
+      (value) => Object
+        .keys(value) // stream delle chiavi contenute in value (riga del dataset)
+        .filter((key) => this.feature.indexOf(key) !== -1
+          || this.target.indexOf(key) !== -1)
+        .reduce((obj, key) => ({ // riduce l'array di chiavi in un object literal
+          ...obj,
+          [key]: value[key],
+        }), {})
+      ,
+    );
+  }
+}
+
+module.exports = DataModel;
---
+++ client/src/model/ForceFieldModel.js
@@ -0,0 +1,51 @@
+const DataModel = require('./DataModel');
+
+class ForceFieldModel {
+  constructor(dataModel) {
+    this._dataModel = dataModel;
+  }
+
+  get dataModel() {
+    return this._dataModel;
+  }
+
+  set dataModel(value) {
+    this._dataModel = value;
+  }
+
+  getNodes() {
+    return this.dataModel.getFeatureColumns()
+      .map((value, index) => ({
+        id: index,
+        features: Object.keys(value)
+          .map((key) => `${key}: ${value[key]}`)
+          .join(', '),
+      }));
+  }
+
+  getLinks(distanceFn) {
+    const links = [];
+    const targetColumns = this.dataModel.getTargetColumns();
+    targetColumns.forEach((value, index) => {
+      for (let i = index + 1; i < targetColumns.length; i++) {
+        links.push(
+          {
+            source: index,
+            target: i,
+            value: distanceFn(Object.values(value), Object.values(targetColumns[i])),
+          },
+        );
+      }
+    });
+    return links;
+  }
+
+  getPreparedDataset(distanceFn) {
+    return {
+      nodes: this.getNodes(),
+      links: this.getLinks(distanceFn),
+    };
+  }
+}
+
+module.exports = ForceFieldModel;
