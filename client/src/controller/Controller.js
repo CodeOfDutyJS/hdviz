40959e1e8ea37ec26d76ac58ee6cbf3e6126ff16
Damiano Zanardo <damiano.zanardo@gmail.com>
2021-03-19 14:39:04 +0000

    fix comment

--- client/src/controller/Controller.js
+++ client/src/controller/Controller.js
@@ -113,7 +113,7 @@ class Controller {
       });
     });
 
-    // Set data al modello
+    // Set data to model
     const _data = await parseFile(file);
     // this.model.reset();
     this.model.dataset = _data;
