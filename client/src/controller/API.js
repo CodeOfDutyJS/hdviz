df472cf6d8cd5ce5265428d36785d0e3a597842e
falsettimatteo <matteofalsetti25gmail.com>
2021-04-09 09:38:01 +0200

    risettato API.js

--- client/src/controller/API.js
+++ client/src/controller/API.js
@@ -1,32 +1,16 @@
 const API_URL = 'http://localhost:1337/api';
 
-
 export async function getDatabases() {
   const response = await fetch(`${API_URL}/getDatabases`);
   return response.json();
 }
 
 export async function getTables(db) {
-  const response = await fetch(`${API_URL}/getTables`, {
-    method: "POSt",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({'name': db}),
-  })
+  const response = await fetch(`${API_URL}/getTables?dbname=${db}`);
   return response.json();
 }
 
 export async function getData(db, table) {
-  const response = await fetch(`${API_URL}/getData`,{
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON:stringify({
-      "name": db,
-      "table": table
-    })
-  });
+  const response = await fetch(`${API_URL}/getData?dbname=${db}&dbtable=${table}`);
   return response.json();
 }
