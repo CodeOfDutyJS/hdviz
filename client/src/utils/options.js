5203171dcd09cf21b3fee8e2ece69fab01384db4
AndreaBreggion <andreabreg99@gmail.com>
2021-04-10 18:09:04 +0200

    forgot to add file

---
+++ client/src/utils/options.js
@@ -0,0 +1,12 @@
+export const DistanceType = {
+  EUCLIDEAN: 'euclidean',
+  MANHATTAN: 'manhattan',
+  PEARSONS: 'pearsons',
+};
+
+export const ClusteringType = {
+  FEATURE: 'feature',
+  COMPLETE: 'complete',
+  SINGLE: 'single',
+  UPGMA: 'upgma',
+};
