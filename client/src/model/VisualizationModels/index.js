6e1db5402bb8fd3e29b574f67c767927e16db776
AndreaBreggion <andreabreg99@gmail.com>
2021-04-15 15:56:33 +0200

    index files added

---
+++ client/src/model/VisualizationModels/index.js
@@ -0,0 +1,13 @@
+import CorrelationHeatmapModel from './CorrelationHeatmapModel';
+import ForceFieldModel from './ForceFieldModel';
+import HeatMapModel from './HeatMapModel';
+import LinearProjectionModel from './LinearProjectionModel';
+import ScatterPlotMatrixModel from './ScatterPlotMatrixModel';
+
+export {
+  CorrelationHeatmapModel,
+  ForceFieldModel,
+  HeatMapModel,
+  LinearProjectionModel,
+  ScatterPlotMatrixModel,
+};
