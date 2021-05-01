a40946f7953a1ec0ae9d502e4d2d5dfa6dd4059c
strang3nt <flori995@live.com>
2021-05-01 13:48:04 +0200

    Risolti problemi linting

--- server/src/modules/MongoDB.js
+++ server/src/modules/MongoDB.js
@@ -1,39 +1,38 @@
-/* eslint-disable new-cap */
-/* eslint-disable max-len */
-/* eslint-disable linebreak-style */
-const mongo = require('mongodb').MongoClient;
-const Database = require('./Database');
+import { MongoClient } from 'mongodb';
+import Database from './Database';
 
-module.exports = class MongoDb extends Database {
+export default class MongoDb extends Database {
   constructor(config) {
     super(config);
     this.uri = config.DB_Address + config.DB_Name;
   }
 
   async connectTo() {
-    return new Promise((resolve, reject) => {
-
-      resolve(new mongo.connect(this.uri, {
+    return new Promise((resolve) => {
+      resolve(MongoClient.connect(this.uri, {
         useUnifiedTopology: true,
       }));
     });
   }
 
   async getTables(conn) {
-    return Promise.resolve(conn.db(this.config.DB_Name).listCollections().toArray()).then((res) => res.map( (out) => out.name));
+    return Promise
+      .resolve(
+        conn.db(this.config.DB_Name)
+          .listCollections()
+          .toArray(),
+      ).then((res) => res.map((out) => out.name));
   }
 
   async getData(conn, collectionName) {
-    return new Promise((resolve,reject) => {
-      //const coll = Promise.resolve(conn.db(this.config.DB_Name));
+    return new Promise((resolve) => {
+      // const coll = Promise.resolve(conn.db(this.config.DB_Name));
       resolve(conn.db(this.config.DB_Name).collection(collectionName).find().toArray());
-
-
     });
-
   }
 
+  // eslint-disable-next-line class-methods-use-this
   async endConnection(conn) {
     conn.close();
   }
-};
+}
