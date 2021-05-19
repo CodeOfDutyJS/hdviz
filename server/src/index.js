52f439b525d199ff6e54aa7e182e9cd568a1e235
falsettimatteo <74828514+falsettimatteo@users.noreply.github.com>
2021-05-19 09:39:43 +0200

    corretto merge con server

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const { findDB, getFiles, selectConfig } = require('./utils');
 
 const app = express();
@@ -20,6 +21,49 @@ app.get('/api/getDatabases', (req, res) => { // controllare se qui deve tornare 
       error: 1,
       msg: 'No configuration file found',
     });
+    return;
+  }
+  res.json([{ databases }]);
+});
+
+app.get('/api/getTable', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const { dbname } = req.query;
+  const configurazione = selectConfig(dbname);
+
+  if (configurazione === 0) {
+    res.json({
+      error: 1,
+      msg: 'No configuration file found',
+    });
+    return;
+  }
+  try {
+    const db = findDB(configurazione);
+    const tables = await db.getTables();
+    res.json(tables);
+  } catch (e) {
+    res.json({
+      error: 1,
+      msg: e,
+    });
+  }
+});
+
+app.get('/api/getData/', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const { dbname } = req.query;
+  const { dbtable } = req.query;
+  const configurazione = selectConfig(dbname);
+
+  if (configurazione === 0) {
+    res.json({
+      error: 1,
+      msg: 'No configuration found',
+    });
+    return;
+  }
+  try {
     const db = await findDB(configurazione);
     const data = await db.getData(dbtable);
     res.json(data);
